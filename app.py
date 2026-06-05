420d6ae2c57a5b0654cc60b4811d9b245d990274
Ankit Kumar <ankitkumar965106@gmail.com>
2026-06-05 14:59:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1982,7 +1982,6 @@ def api_get_conversations():
 
 @app.route('/api/messages/<int:uid>', methods=['GET'])
 def api_get_messages(uid):
-    """Get chat messages with a user"""
 
     if 'user_id' not in session:
         return jsonify({'error': 'Unauthorized'}), 401
@@ -1993,60 +1992,49 @@ def api_get_messages(uid):
     cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
 
     if after > 0:
-
         cur.execute("""
             SELECT c.id, c.sender_id, c.message, c.file, c.seen, c.time,
                    c.starred, c.pinned, c.reaction,
                    u.name AS sender_name, u.profile_pic
             FROM chat c
             JOIN users u ON c.sender_id = u.user_id
-            WHERE (
-                    (c.sender_id=%s AND c.receiver_id=%s)
-                 OR (c.sender_id=%s AND c.receiver_id=%s)
-                  )
+            WHERE ((c.sender_id=%s AND c.receiver_id=%s)
+                OR (c.sender_id=%s AND c.receiver_id=%s))
               AND c.id > %s
             ORDER BY c.id ASC
         """, (my_id, uid, uid, my_id, after))
-
     else:
-
         cur.execute("""
             SELECT c.id, c.sender_id, c.message, c.file, c.seen, c.time,
                    c.starred, c.pinned, c.reaction,
                    u.name AS sender_name, u.profile_pic
             FROM chat c
             JOIN users u ON c.sender_id = u.user_id
-            WHERE (
-                    (c.sender_id=%s AND c.receiver_id=%s)
-                 OR (c.sender_id=%s AND c.receiver_id=%s)
-                  )
+            WHERE (c.sender_id=%s AND c.receiver_id=%s)
+               OR (c.sender_id=%s AND c.receiver_id=%s)
             ORDER BY c.id ASC
         """, (my_id, uid, uid, my_id))
-messages = cur.fetchall()
 
-for m in messages:
-    if m['time']:
-        m['time'] = m['time'].strftime('%I:%M %p')
+    messages = cur.fetchall()
 
-# Mark as seen only if new messages exist
-if messages:
-    cur.execute(
-        """
-        UPDATE chat
-        SET seen = TRUE
-        WHERE receiver_id=%s
-        AND sender_id=%s
-        AND seen=FALSE
-        """,
-        (my_id, uid)
-    )
+    for m in messages:
+        if m['time']:
+            m['time'] = m['time'].strftime('%I:%M %p')
 
-    mysql.connection.commit()
+    if messages:
+        cur.execute("""
+            UPDATE chat
+            SET seen = TRUE
+            WHERE receiver_id=%s
+              AND sender_id=%s
+              AND seen=FALSE
+        """, (my_id, uid))
+        mysql.connection.commit()
 
-return jsonify({
-    'messages': messages,
-    'my_id': my_id
-}), 200
+    return jsonify({
+        'messages': messages,
+        'my_id': my_id
+    }), 200
 @app.route('/api/messages/<int:uid>', methods=['POST'])
 def api_send_message(uid):
     """Send a message via AJAX"""
